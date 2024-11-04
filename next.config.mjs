53ab7deeeb4e2d585d1a21c2d5e52d547ae28c99
Bharat Shetty B <bharat.shetty@gmail.com>
2024-11-04 05:02:39 +0000

    Fix logo config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+      unoptimized: true  // only if needed
+    },
+};
 
 export default nextConfig;
